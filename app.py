eb13a61b7018b356a103ac3ef718076f94dfd6f0
xinjuan Deng <dengxinj@gmail.com>
2020-12-14 22:12:47 -0500

    working on the homepage form setup now

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import requests
 
 def request_amazon(amazonkeyword):
 
-    """this will return jason for the product keyword"""
+    """this will return json for the product keyword"""
 
     url = "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search"
 
@@ -26,13 +26,13 @@ def request_amazon(amazonkeyword):
 
 
 
+def request_target(targetkeyword):
+    """this will return json for the product keyword"""
 
-@app.route('/')
-def home_page():
-     """targetApi"""
+    """targetApi"""
     url = "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/product/search"
 
-    querystring = {"store_id":"3991","keyword":"iphone 7","sponsored":"1","limit":"50","offset":"0"}
+    querystring = {f"store_id":"3991","keyword": {targetkeyword},"sponsored":"1","limit":"50","offset":"0"}
 
     headers = {
     'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
@@ -42,7 +42,38 @@ def home_page():
     response = requests.request("GET", url, headers=headers, params=querystring)
 
     result = response.text
-    return render_template('home.html',result = result)
+
+    return result
+
+
+def request_walmart(walmartkeyword):
+
+    """get walmart json information"""
+
+    url = "https://search-walmart.p.rapidapi.com/"
+    
+    querystring = {f"name":{walmartkeyword}}
+
+    headers = {
+    'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+    'x-rapidapi-host': "search-walmart.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    result = response.text
+
+    return result
+    
+
+
+
+
+@app.route('/')
+def home_page():
+    """ render the basic homepage"""
+
+    return render_template('home.html')
 
 
 
