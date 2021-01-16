4a17538acba7bda7776aa85d1b39f95fd9a33d89
xinjuan deng <dengxinj@gmail.com>
2021-01-15 23:54:04 -0500

    working on reviews ui

--- static/truedeal.js
+++ static/truedeal.js
@@ -1,8 +1,20 @@
 //window.alert("hello!")
 
-function hidden() {
-  var x = document.getElementById("reviews_by_asin");
- 
-  x.style.visibility ="hidden";
+/*var y = document.getElementById("showbtn");
+var x = document.getElementById("reviews_by_asin");
 
+
+
+
+document.getElementById("reviewbtn").addEventListener("click", function(){
+  y.style.display = "block";
+  x.style.display = "none"
 }
+);
+
+
+document.getElementById("showbtn").addEventListener("click", function(){
+  y.style.display = "none";
+  x.style.display = "block"
+}
+);*/
