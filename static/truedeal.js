08bea979dfe3c9d9d7cf2c81569d1767540918dc
xinjuan deng <dengxinj@gmail.com>
2021-01-15 23:14:12 -0500

    working on ui

--- static/truedeal.js
+++ static/truedeal.js
@@ -1,43 +1,8 @@
-/*window.alert("hello!")
+//window.alert("hello!")
 
-/*function processForm(evt){
-    evt.preventDefault();
+function hidden() {
+  var x = document.getElementById("reviews_by_asin");
  
+  x.style.visibility ="hidden";
 
-        method: "POST",
-    url: "/api/search",
-    contentType: "application/json",
-    data: JSON.stringify({
-      //brand: $("#brand").val(),
-      productname: $("#productname").val(),
-      //version: $("#version").val(),
-    }),
-    success: handleResponse // if success call handleResponse function
-
-    })
 }
-
-function handleResponse(resp){
-    if ("errors" in resp) {
-        // received errors from API:
-        //   for each error, put message next to corresponding field
-    
-        for (let fld in resp.errors) {
-          $(`#${fld}-err`).text(resp.errors[fld]);
-        }
-      }
-    
-      else {
-    
-        $("#result").text(resp);
-      }
-  
-} 
-
-
-
-$("#search-form").on("submit",processForm)
-
-
-
-*/
