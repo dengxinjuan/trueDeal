b1c1a3d7cab5a51b8dd4840ff724485e7f2ae245
xinjuan deng <dengxinj@gmail.com>
2021-02-02 22:15:21 -0500

    reformat

--- app.py
+++ app.py
@@ -1,56 +1,57 @@
-from flask import Flask,render_template,request,jsonify,redirect,flash,session,send_from_directory
+from flask import Flask, render_template, request, jsonify, redirect, flash, session, send_from_directory
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db,connect_db,User,ShoppingList,UserFav
+from models import db, connect_db, User, ShoppingList, UserFav
 
 import os
 import json
 import requests
 
-from forms import LoginForm,RegisterForm,AsinSearchForm, ReviewsByAsinForm, ShoppingListForm
+from forms import LoginForm, RegisterForm, AsinSearchForm, ReviewsByAsinForm, ShoppingListForm
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-#connect database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "postgres:///truedeal")
+# connect database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', "postgres:///truedeal")
 # compress the warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#echo the sql command
+# echo the sql command
 app.config['SQLALCHEMY_ECHO'] = True
-#CONFIG THE DEBUGTOOL
+# CONFIG THE DEBUGTOOL
 app.debug = True
 # config heroku variable, secret key
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','YOUDONTKNOWTHAT')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'YOUDONTKNOWTHAT')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #toolbar = DebugToolbarExtension(app)
 
-#connect the app, database
+# connect the app, database
 connect_db(app)
-#create table
+# create table
 db.create_all()
 
-##function to search 
-def request_amazon(amazonkeyword,country):
+# function to search
 
+
+def request_amazon(amazonkeyword, country):
     """this will return json for the product keyword"""
 
     url = "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search"
 
-    querystring = {f"keyword": {amazonkeyword},"country":{country},"category":"aps"}
+    querystring = {f"keyword": {amazonkeyword},
+                   "country": {country}, "category": "aps"}
 
     headers = {
-    'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
-    'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
+        'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+        'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
     result = json.loads(response.text)
-   
+
     return result['products']
-
-
-
 
 
 def request_target(targetkeyword):
@@ -59,14 +60,16 @@ def request_target(targetkeyword):
     """targetApi"""
     url = "https://target-com-store-product-reviews-locations-data.p.rapidapi.com/product/search"
 
-    querystring = {f"store_id":"3991","keyword": {targetkeyword},"sponsored":"1","limit":"50","offset":"0"}
+    querystring = {f"store_id": "3991", "keyword": {
+        targetkeyword}, "sponsored": "1", "limit": "50", "offset": "0"}
 
     headers = {
-    'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
-    'x-rapidapi-host': "target-com-store-product-reviews-locations-data.p.rapidapi.com"
+        'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+        'x-rapidapi-host': "target-com-store-product-reviews-locations-data.p.rapidapi.com"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
 
     result = response.text
 
@@ -74,62 +77,55 @@ def request_target(targetkeyword):
 
 
 def request_walmart(walmartkeyword):
-
     """get walmart json information"""
 
     url = "https://search-walmart.p.rapidapi.com/"
-    
-    querystring = {f"name":{walmartkeyword}}
+
+    querystring = {f"name": {walmartkeyword}}
 
     headers = {
-    'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
-    'x-rapidapi-host': "search-walmart.p.rapidapi.com"
+        'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+        'x-rapidapi-host': "search-walmart.p.rapidapi.com"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
 
     result = response.text
 
     return result
-    
+
 
 ##########
-
-
 
 
 @app.route('/')
 def home_page():
     """ render the basic homepage"""
 
-
     return render_template('home.html')
-
-
-
 
 
 @app.route('/search')
 def search_result():
     """redner search result"""
     productname = request.args["productname"]
-    brand=request.args["brand"]
-    version=request.args["version"]
+    brand = request.args["brand"]
+    version = request.args["version"]
     country = request.args["country"]
     searchterm = brand+productname+version
-    result = request_amazon(searchterm,country)
+    result = request_amazon(searchterm, country)
 
     #username = session['username']
-    
-  
+
     #walmartresult =request_walmart(searchterm)
-    #targetresult=request_target(searchterm)
-    return render_template('search.html',result=result, len=len(result))
+    # targetresult=request_target(searchterm)
+    return render_template('search.html', result=result, len=len(result))
 
 
 ##########User ####
 
-@app.route('/login', methods=["GET","POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login_page():
     """render login form"""
     form = LoginForm()
@@ -137,24 +133,20 @@ def login_page():
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        user = User.authenticate(username,password)
+        user = User.authenticate(username, password)
 
         if user:
-            session["username"] = user.username #keep logged in
+            session["username"] = user.username  # keep logged in
             flash(f"welcome! Dear {username}!")
             return redirect(f"users/{username}")
         else:
             form.username.errors = ["Password/Username Wrong!!!"]
-            return render_template("login.html",form=form)
-        
+            return render_template("login.html", form=form)
 
-    return render_template('login.html',form=form)
-
+    return render_template('login.html', form=form)
 
 
-
-
-@app.route("/signup", methods=['GET','POST'])
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
     """render signup form"""
 
@@ -165,18 +157,17 @@ def signup():
         password = form.password.data
         profile_img = form.profile_img.data
 
-        user = User.register(username,password,profile_img)
+        user = User.register(username, password, profile_img)
         db.session.add(user)
 
         db.session.commit()
         session['username'] = user.username
 
         return redirect(f"/users/{username}")
-    
-    else:
-        
-        return render_template("signup.html",form=form)
 
+    else:
+
+        return render_template("signup.html", form=form)
 
 
 @app.route("/logout")
@@ -194,24 +185,26 @@ def show_user(username):
     if 'username' not in session or username != session['username']:
         flash("You must be logged in to view!")
         return redirect("/")
-    
+
     else:
-        
+
         user = User.query.filter_by(username=username).first_or_404()
         profile_img = user.profile_img
-        lists = ShoppingList.query.filter_by(username = username)
+        lists = ShoppingList.query.filter_by(username=username)
         userfav = user.userfav
-        return render_template("username.html",user=user, profile_img=profile_img, lists=lists,userfav=userfav)
+        return render_template("username.html", user=user, profile_img=profile_img, lists=lists, userfav=userfav)
 
-## user-delete user
+# user-delete user
+
+
 @app.route("/users/<username>/delete", methods=['POST'])
 def delete_user(username):
     """delete user"""
     if 'username' not in session or username != session['username']:
         flash("You must be logged in to view!")
         return redirect("/")
-    
-    else: 
+
+    else:
         user = User.query.filter_by(username=username).first_or_404()
         db.session.delete(user)
         db.session.commit()
@@ -219,84 +212,76 @@ def delete_user(username):
 
         return redirect("/login")
 
-## user-edit userprofile
-## user-add favorite
+# user-edit userprofile
+# user-add favorite
 
 
-##error handle
+# error handle
 
 @app.errorhandler(404)
 def page_not_found(e):
     """customize error page"""
-    return render_template('404.html'),404
-    
+    return render_template('404.html'), 404
 
 
-### seach by asin page
+# seach by asin page
 
-def product_by_asin(asin,country):
+def product_by_asin(asin, country):
     """return product details by asin"""
 
     url = "https://amazon-product-reviews-keywords.p.rapidapi.com/product/details"
-    querystring = {f"asin":{asin},"country":{country}}
-    
+    querystring = {f"asin": {asin}, "country": {country}}
+
     headers = {
-    'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
-    'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
+        'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+        'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
     }
-    
-    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
 
     result = json.loads(response.text)
-    
+
     return result['product']
 
 
-    
-
-
-
-@app.route("/asin",methods=['GET','POST'])
+@app.route("/asin", methods=['GET', 'POST'])
 def search_by_asin():
     """search by asin"""
     form = AsinSearchForm()
     if form.validate_on_submit():
         asin_search_term = form.ASIN.data
         country = form.country.data
-        result = product_by_asin(asin_search_term,country)
-        return render_template('asinresult.html',result=result)
-    
-    return render_template("asin.html",form=form)
+        result = product_by_asin(asin_search_term, country)
+        return render_template('asinresult.html', result=result)
+
+    return render_template("asin.html", form=form)
 
 
+# search reviews by asin
 
-
-
-
-
-### search reviews by asin
-
-def search_reviews(asin,country):
+def search_reviews(asin, country):
     """get reviews by asin"""
 
     url = "https://amazon-product-reviews-keywords.p.rapidapi.com/product/reviews"
-    
-    querystring = {f"asin":{asin},"country":{country},"variants":"1","top":"0"}
-    
-    headers = {
-    'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
-    'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
-    }
-    
-    response = requests.request("GET", url, headers=headers, params=querystring)
 
-    result= json.loads(response.text)
+    querystring = {f"asin": {asin}, "country": {
+        country}, "variants": "1", "top": "0"}
+
+    headers = {
+        'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+        'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
+
+    result = json.loads(response.text)
 
     return result['reviews']
 
 
-
-@app.route("/reviews", methods=['GET','POST'])
+@app.route("/reviews", methods=['GET', 'POST'])
 def reviews_by_asin():
     """return reviews by asin"""
     form = ReviewsByAsinForm()
@@ -304,20 +289,19 @@ def reviews_by_asin():
     if form.validate_on_submit():
         review_search_term = form.reviewsByAsin.data
         country = form.country.data
-        result = search_reviews(review_search_term,country)
-        return render_template('reviewresult.html',result=result)
+        result = search_reviews(review_search_term, country)
+        return render_template('reviewresult.html', result=result)
 
-    return render_template('reviews.html',form=form)
+    return render_template('reviews.html', form=form)
 
 
-
-#### user shopping list 
-@app.route("/users/<username>/shoppinglist/add", methods=["GET","POST"])
+# user shopping list
+@app.route("/users/<username>/shoppinglist/add", methods=["GET", "POST"])
 def new_shopping_list(username):
     """show add-shopping-list-form and process it"""
-    
+
     if 'username' not in session or username != session['username']:
-        
+
         flash("You must be logged in to view!")
         return redirect("/")
 
@@ -327,8 +311,8 @@ def new_shopping_list(username):
         content = form.content.data
         shoppinglist = ShoppingList(
             content=content,
-            done = False,
-            username = username
+            done=False,
+            username=username
         )
         db.session.add(shoppinglist)
         db.session.commit()
@@ -336,8 +320,8 @@ def new_shopping_list(username):
         return redirect(f"/users/{username}")
 
     else:
-        lists = ShoppingList.query.filter_by(username = username)
-        return render_template("shoppinglist.html",form=form,lists=lists)
+        lists = ShoppingList.query.filter_by(username=username)
+        return render_template("shoppinglist.html", form=form, lists=lists)
 
 
 @app.route("/shoppinglist/<int:shoppinglist_id>/delete", methods=["POST"])
@@ -348,7 +332,7 @@ def delete_shopping_list(shoppinglist_id):
     db.session.commit()
 
     return redirect(f"/users/{shoppinglist.username}")
-        
+
 
 @app.route("/shoppinglist/<int:shoppinglist_id>/done")
 def done_shoppinglist(shoppinglist_id):
@@ -358,48 +342,40 @@ def done_shoppinglist(shoppinglist_id):
         return redirect("/")
     if shoppinglist.done:
         shoppinglist.done = False
-    else: 
+    else:
         shoppinglist.done = True
     db.session.commit()
     return redirect(f"/users/{shoppinglist.username}")
 
 
-
-### fix favicon error
+# fix favicon error
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-
-#### add favorite asin 
+# add favorite asin
 @app.route('/addfav/<asin>', methods=["POST"])
 def add_fav(asin):
     """add fav to users"""
 
-    username=session['username']
-    newFav = UserFav(username=username,asin=asin, fav=True )
+    username = session['username']
+    newFav = UserFav(username=username, asin=asin, fav=True)
     db.session.add(newFav)
     db.session.commit()
 
     return redirect(f"/users/{username}")
 
 
-###remove favorite asin, hate this item!
+# remove favorite asin, hate this item!
 
-@app.route('/removefav/<asin>',methods=["POST"])
+@app.route('/removefav/<asin>', methods=["POST"])
 def remove_fav(asin):
     """remove fav to users"""
-    username= session['username']
-    hate = UserFav(username=username,asin=asin,fav=False)
+    username = session['username']
+    hate = UserFav(username=username, asin=asin, fav=False)
     db.session.add(hate)
     db.session.commit()
-    
+
     return redirect(f"/users/{username}")
-    
-
-    
-
-
-
