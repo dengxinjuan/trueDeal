2cb1e80113c4f9e4a0512388a253c3254d7ff320
xinjuan deng <dengxinj@gmail.com>
2021-02-11 14:20:54 -0500

    try working?

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def request_amazon(amazonkeyword, country):
                    "country": {country}, "category": "aps"}
 
     headers = {
-        'x-rapidapi-key': os.getenv("AMAZON_REQUEST_KEY"),
-        'x-rapidapi-host': os.getenv("AMAZON_REQUEST_API")
+        'x-rapidapi-key': "e6001d6072msh1f868436da26ed9p1ce5c5jsnb7b3847e24ce",
+        'x-rapidapi-host': "amazon-product-reviews-keywords.p.rapidapi.com"
     }
 
     response = requests.request(
